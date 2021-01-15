efdb97f9af0a879c7145daf99510b4cdc9cec1bc
Omer Ashraf <17503@northberen.k12.nj.us>
2021-01-15 17:36:33 +0000

    fixing duoble repeat

--- img2text.py
+++ img2text.py
@@ -10,10 +10,8 @@ char_aspect = .6
 input_file, colors, output_width, output_file = sys.argv[1:]
 try:
   ncolors = int(colors)
-  if ncolors not in range(2, 10):
-    raise ValueError
   output_width = int(output_width)
-  if ncolors not in range(2, 10) or output_width not in range(80,121):
+  if ncolors not in range(2, 11) or output_width not in range(80,121):
     raise ValueError
 
   original_img = Image.open(input_file)
@@ -36,6 +34,6 @@ try:
             output += gradient[usable_gradient[value]]
         f.write(output + "\n")
 except:
-  print("Invalid input for colors:acceptable is number range from 2-9 or invalid input for output_width: acceptable is number range from 80-120")
+  print("Invalid input for colors:acceptable is number range from 2-10 or invalid input for output_width: acceptable is number range from 80-120")
 
 
