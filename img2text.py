48342cf57edb18d457e1b78efe59c7fe00707824
Alan Grisales <11214@northbergen.k12.nj.us>
2021-01-18 00:15:53 +0000

    iss67

--- img2text.py
+++ img2text.py
@@ -5,11 +5,28 @@ from PIL import Image
 # python img/surprised_pikachu.jpg 10 120 img/surprised_pikachu.txt
 
 char_aspect = .6
+gradient = " .:-=+*#%@"
 
 # parsing command line inputs
 input_file, colors, output_width, output_file = sys.argv[1:]
-ncolors = int(colors)
-output_width = int(output_width)
+
+try:
+    ncolors = int(colors)
+    if (ncolors <= 0) or (ncolors > len(gradient)):
+        print("Input has to be between 0 and 10 for the colors")
+        sys.exit()
+except ValueError:
+    print("Input has to be a number")
+    sys.exit()
+
+try:
+    output_width = int(output_width)
+    if (output_width < 80) or (output_width > 240):
+        print("Input has to be between 80 and 240")
+        sys.exit()
+except ValueError:
+    print("Input has to be number")
+    sys.exit()
 
 original_img = Image.open(input_file)
 original_width, original_height = original_img.size
@@ -21,7 +38,6 @@ processed_img = img_bw_quantized.resize((output_width, int(scaling_factor * orig
 
 img_array = np.array(processed_img)
 
-gradient = " .:-=+*#%@"
 usable_gradient = [int(round(i)) for i in np.linspace(0, len(gradient) - 1, ncolors)]
 
 with open(output_file, "w") as f:
