d5c2b21e62f885b64b345569bbaa8d2f435ace21
Nayelly Marin-Klinkert <18276@northbergen.k12.nj.us>
2021-01-20 15:46:59 +0000

    Resolves denricoNBHS#67

--- img2text.py
+++ img2text.py
@@ -8,19 +8,32 @@ char_aspect = .6
 
 # parsing command line inputs
 input_file, colors, output_width, output_file = sys.argv[1:]
-ncolors = int(colors)
-output_width = int(output_width)
+
+gradient = " .:-=+*#%@"
+try:
+    ncolors = int(colors)
+    if (ncolors <= 0) or (ncolors > len(gradient)):
+        print("Invalid input. The number of colors has to be between 0 and 10")
+        sys.exit()
+except ValueError:
+    print("The number of colors has to be a number")
+    sys.exit()
+
+try:
+    output_width = int(output_width)
+    if (output_width < 80) or (output_width > 240):
+        print("Invalid input. The width has to be between 80 and 240")
+        sys.exit()
+except ValueError:
+    print("The width has to be a number")
+    sys.exit()
 
 original_img = Image.open(input_file)
 original_width, original_height = original_img.size
 
-img_bw_quantized = original_img.convert("L").quantize(colors=ncolors)
-
-scaling_factor = output_width / original_width
-processed_img = img_bw_quantized.resize((output_width, int(scaling_factor * original_height * char_aspect)))
-
 img_array = np.array(processed_img)
 
+<<<<<<< d0489702cfbb5732da1c321a6cbb2e33abb148ec
 gradient = " .:-=+*#%@"
 usable_gradient = [int(round(i)) for i in np.linspace(0, len(gradient) - 1, ncolors)]
 
@@ -30,6 +43,4 @@ with open(output_file, "w") as f:
         for value in row:
             output += gradient[usable_gradient[value]]
         f.write(output + "\n")
-
-
 
