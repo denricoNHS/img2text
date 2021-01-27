69c3dda34d7a8d09fe7ab494c5e47b997446ec62
Alan Grisales <11214@northbergen.k12.nj.us>
2021-01-27 04:00:52 +0000

    changed sys.argv

--- img2text.py
+++ img2text.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import numpy as np
 from PIL import Image
 
@@ -7,24 +7,27 @@ from PIL import Image
 char_aspect = .6
 
 # parsing command line inputs
-input_file, colors, output_width, output_file = sys.argv[1:]
-ncolors = int(colors)
-output_width = int(output_width)
+parser = argparse.ArgumentParser(description='saving your inputs')
+parser.add_argument('input_file', help ='the loaction of the image you input')
+parser.add_argument("color", type = int, help ='is an integer between 2 and 10 that represents the amount of shades')
+parser.add_argument("output_width", type = int, help ='is an integer between 80 and 240 that represents the width you wish for the image to be')
+parser.add_argument("output_file", help ='location of the file where the output will be')
+args = parser.parse_args()
 
-original_img = Image.open(input_file)
+original_img = Image.open(args.input_file)
 original_width, original_height = original_img.size
 
-img_bw_quantized = original_img.convert("L").quantize(colors=ncolors)
+img_bw_quantized = original_img.convert("L").quantize(args.color)
 
-scaling_factor = output_width / original_width
-processed_img = img_bw_quantized.resize((output_width, int(scaling_factor * original_height * char_aspect)))
+scaling_factor = args.output_width / original_width
+processed_img = img_bw_quantized.resize((args.output_width, int(scaling_factor * original_height * char_aspect)))
 
 img_array = np.array(processed_img)
 
 gradient = " .:-=+*#%@"
-usable_gradient = [int(round(i)) for i in np.linspace(0, len(gradient) - 1, ncolors)]
+usable_gradient = [int(round(i)) for i in np.linspace(0, len(gradient) - 1, args.colors)]
 
-with open(output_file, "w") as f:
+with open(args.output_file, "w") as f:
     for row in img_array:
         output = ""
         for value in row:
