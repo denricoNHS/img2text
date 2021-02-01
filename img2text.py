b29777f220436a33cf4d01d053a940849c0c822c
Nicole Renteria <44007201@northbergen.k12.nj.us>
2021-02-01 03:31:18 +0000

    pog lol

--- img2text.py
+++ img2text.py
@@ -1,4 +1,5 @@
 import argparse
+import requests
 import numpy as np
 from PIL import Image
 
@@ -12,9 +13,12 @@ parser.add_argument("input_file", type=str, help="path to original image")
 parser.add_argument("colors", type=int, help="number of grayscale values to use on output")
 parser.add_argument("output_width", type=int, help="width of output in characters")
 parser.add_argument("output_file", type=str, help="path to write text file")
+parser.add_argument('-w', '--web', help = "link to the original image", action='store_true')
 args = parser.parse_args()
-
-original_img = Image.open(args.input_file)
+if args.web == True:
+  original_img = Image.open(requests.get(args.input_file, stream=True).raw)
+else:
+  original_img = Image.open(args.input_file)
 original_width, original_height = original_img.size
 
 img_bw_quantized = original_img.convert("L").quantize(colors=args.colors)
@@ -33,5 +37,3 @@ with open(args.output_file, "w") as f:
         for value in row:
             output += gradient[usable_gradient[value]]
         f.write(output + "\n")
-
-
